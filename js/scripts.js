6443d79c93cbd591b36f6f40d699bf1916ff16cb
Scott Mackey <mackey.scott23@gmail.com>
2022-10-10 10:20:16 +0100

    Some more Navbar styling changes

--- js/scripts.js
+++ js/scripts.js
@@ -173,12 +173,12 @@ function showSlides2(n) {
 
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
-  document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("side-wrapper").style.marginLeft = "250px";
+  document.getElementById("mySidebar").style.width = "15%";
+  document.getElementById("side-wrapper").style.display = "none";
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("side-wrapper").style.marginLeft = "0";
+  document.getElementById("side-wrapper").style.display = "block";
 }
