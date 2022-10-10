156b17b552196b6a52e72aef61f949daf52ab234
Scott Mackey <mackey.scott23@gmail.com>
2022-10-10 10:25:45 +0100

    removed the side-wrapper when menu open

--- js/scripts.js
+++ js/scripts.js
@@ -174,11 +174,14 @@ function showSlides2(n) {
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
   document.getElementById("mySidebar").style.width = "15%";
+  document.getElementById("mySidebar").style.paddingright = "1%";
   document.getElementById("side-wrapper").style.display = "none";
+
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("mySidebar").style.paddingright = "0%";
   document.getElementById("side-wrapper").style.display = "block";
 }
