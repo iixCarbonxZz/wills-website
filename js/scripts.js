09aa9a3ccfe6b25cb57a9999320523191ed4ceb5
Scott Mackey <mackey.scott23@gmail.com>
2022-10-19 09:44:55 +0100

    Tried switching the Gallery up

--- js/scripts.js
+++ js/scripts.js
@@ -43,7 +43,8 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
@@ -54,36 +55,7 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-}
-
-
-let slideIndex2 = 1;
-showSlides2(slideIndex);
-
-// Next/previous controls
-function plusSlides2(n) {
-  showSlides2(slideIndex2 += n);
-}
-
-// Thumbnail image controls
-function currentSlide2(n) {
-  showSlides2(slideIndex2 = n);
-}
-
-function showSlides2(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides2");
-  let dots = document.getElementsByClassName("dot2");
-  if (n > slides.length) {slideIndex2 = 1}
-  if (n < 1) {slideIndex2 = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex2-1].style.display = "block";
-  dots[slideIndex2-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
 }
 
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
