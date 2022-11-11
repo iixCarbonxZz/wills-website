224087e96ae869056a33f24e6b40aef4cddc1b50
Scott Mackey <mackey.scott23@gmail.com>
2022-11-11 17:10:58 +0000

    Mobile navbar change 2 electric boogaloo

--- js/scripts.js
+++ js/scripts.js
@@ -45,7 +45,7 @@ function openNav() {
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
 function closeNav() {
-  document.getElementById("mySidebar").style.width = "100%";
+  document.getElementById("mySidebar").style.width = "0%";
   document.getElementById("mySidebar").style.paddingRight = "0%";
   document.getElementById("side-wrapper").style.display = "block";
 }
