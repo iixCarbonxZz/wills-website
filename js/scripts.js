c864cc76b86b361d67f5371a99d96f107b11dc2e
Scott Mackey <mackey.scott23@gmail.com>
2022-11-11 17:10:05 +0000

    Mobile navbar change

--- js/scripts.js
+++ js/scripts.js
@@ -4,29 +4,6 @@ function backTop(){
 
 //document.getElementById("top").onclick = backTop(); //Calls the backTop function when clicked.
 
-function changeButtons(){ //The following function changes whats in the buttons depending on screen size from words to icons.
-  if(window.innerWidth >= 1000){ //Desktop Resolution
-    document.getElementById("menu").innerHTML = "&#9776; Menu";
-    document.getElementById("home").innerHTML = "<img class='icon' src='images/home-icon.png' alt='Home Button'> Home";
-    document.getElementById("Test1").innerHTML = "<img class='icon' src='images/test1-icon.png' alt='Bookings Button'> Treatments";
-    document.getElementById("location").innerHTML = "<img class='icon' src='images/location-icon.png' alt='Location Button'> Find Us";
-    document.getElementById("gallery").innerHTML = "<img class='icon' src='images/gallery-icon.png' alt='Gallery Button'> Gallery";
-    document.getElementById("policies").innerHTML = "<img class='icon' src='images/policy-icon.png' alt='Policy Button'> Policies";
-  //  document.getElementById("top").innerHTML = "<img class='icon' src='images/up-icon.png' alt='Back to top'> Back to Top";
-  }
-  else if(window.innerWidth < 1000){ //Anyhting smaller than a desktop
-    document.getElementById("menu").innerHTML = "&#9776;";
-    document.getElementById("home").innerHTML = "<img class='icon' src='images/home-icon.png' alt='Home Button'>";
-    document.getElementById("Test1").innerHTML = "<img class='icon' src='images/test1-icon.png' alt='Bookings Button'>";
-    document.getElementById("location").innerHTML = "<img class='icon' src='images/location-icon.png' alt='Location Button'>";
-    document.getElementById("gallery").innerHTML = "<img class='icon' src='images/gallery-icon.png' alt='Gallery Button'>";
-    document.getElementById("policies").innerHTML = "<img class='icon' src='images/policy-icon.png' alt='Policy Button'>";
-    //document.getElementById("top").innerHTML = "<img class='icon' src='images/up-icon.png' alt='Back to top'>";
-  }
-}
-document.addEventListener("DOMContentLoaded", changeButtons); //When the page loads runs the changeButtons function.
-window.onresize = changeButtons; //When they resize the window it runs the changeButtons function.
-
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -68,7 +45,7 @@ function openNav() {
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
 function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("mySidebar").style.width = "100%";
   document.getElementById("mySidebar").style.paddingRight = "0%";
   document.getElementById("side-wrapper").style.display = "block";
 }
