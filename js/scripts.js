047410ac0be91998c15c54b889a87d90ca33249a
Scott Mackey <mackey.scott23@gmail.com>
2022-11-21 12:51:58 +0000

    Updated this repo with all the latest changes

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,18 @@ function backTop(){
 function changeButtons(){ //The following function changes whats in the buttons depending on screen size from words to icons.
   console.log("changeButtons()");
   if(window.innerWidth >= 1000){ //Desktop Resolution
-    document.getElementById("menu").innerHTML = "&#9776; Menu";
-    document.getElementById("home").innerHTML = "<img class='icon' src='images/home-icon.png' alt='Home Button'> Home";
-    document.getElementById("Test1").innerHTML = "<img class='icon' src='images/test1-icon.png' alt='Bookings Button'> Treatments";
-    document.getElementById("location").innerHTML = "<img class='icon' src='images/location-icon.png' alt='Location Button'> Find Us";
-    document.getElementById("gallery").innerHTML = "<img class='icon' src='images/gallery-icon.png' alt='Gallery Button'> Gallery";
-    document.getElementById("policies").innerHTML = "<img class='icon' src='images/policy-icon.png' alt='Policy Button'> Policies";
+    document.getElementById("menu").innerHTML = "&#9776;";
+  //  document.getElementById("home").innerHTML = "<img class='icon' src='images/home-icon.png' alt='Home Button'> Home";
+   // document.getElementById("Test1").innerHTML = "<img class='icon' src='images/test1-icon.png' alt='Bookings Button'> Treatments";
+  //  document.getElementById("location").innerHTML = "<img class='icon' src='images/location-icon.png' alt='Location Button'> Find Us";
+  //  document.getElementById("gallery").innerHTML = "<img class='icon' src='images/gallery-icon.png' alt='Gallery Button'> Gallery";
+ //   document.getElementById("policies").innerHTML = "<img class='icon' src='images/policy-icon.png' alt='Policy Button'> Policies";
+      document.getElementById("home").innerHTML = "Home";
+    document.getElementById("Test1").innerHTML = "Treatments";
+    document.getElementById("prices").innerHTML = "Prices";
+    document.getElementById("location").innerHTML = "Find Us";
+    document.getElementById("gallery").innerHTML = "Gallery";
+    document.getElementById("policies").innerHTML = "Policies";
   //  document.getElementById("top").innerHTML = "<img class='icon' src='images/up-icon.png' alt='Back to top'> Back to Top";
   }
   else if(window.innerWidth < 1000){ //Anyhting smaller than a desktop
@@ -20,6 +26,7 @@ function changeButtons(){ //The following function changes whats in the buttons 
     document.getElementById("mySidebar").style.width = "99%";
     document.getElementById("home").innerHTML = "Home";
     document.getElementById("Test1").innerHTML = "Treatments";
+    document.getElementById("prices").innerHTML = "Prices";
     document.getElementById("location").innerHTML = "Find Us";
     document.getElementById("gallery").innerHTML = "Gallery";
     document.getElementById("policies").innerHTML = "Policies";
